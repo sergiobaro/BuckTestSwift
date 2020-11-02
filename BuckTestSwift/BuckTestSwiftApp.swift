21c697b4da4819fa96e0470cf37d8a604bb34132
Sergio Baró <serbarsim@gmail.com>
2020-11-02 08:39:54 +0100

    Initial Commit

---
+++ BuckTestSwift/BuckTestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuckTestSwiftApp.swift
+//  BuckTestSwift
+//
+//  Created by Sergio Baro on 02/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BuckTestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuckTestSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuckTestSwift
+//
+//  Created by Sergio Baro on 02/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
